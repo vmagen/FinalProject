3a45327519e383135dfdd050d5b0c638c91c1870
RonNavon <ronyhezkel@gmail.com>
2021-02-21 19:45:31 +0200

    api

---
+++ arvinoAPI/WebApi/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DATA.EF;
+using WebApi.DTO;
+using System.Data.Entity;
+
+namespace WebApi.Models
+{
+    public class UserModel
+    {
+
+        public static RV_User GetUser(string email, arvinoDbContext db)
+        {
+            return db.RV_User.SingleOrDefault(x => x.email == email);
+        }
+    }
+}
