b763f075e46b93348a63e0205ae3975501a00379
RonNavon <ronyhezkel@gmail.com>
2021-02-28 20:44:04 +0200

    1

---
+++ arvinoAPI/WebApi/DTO/AreaCategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DATA.EF;
+
+namespace WebApi.DTO
+{
+    public class AreaCategoryDTO
+    {
+        public int areaId;
+        public string areaName;
+    }
+}
--- arvinoAPI/WebApi/Models/AreaCategoryModel.cs
+++ arvinoAPI/WebApi/Models/AreaCategoryModel.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DATA.EF;
+using WebApi.DTO;
+using System.Data.Entity;
 
 namespace WebApi.Models
 {
     public class AreaCategoryModel
     {
+        public static List<AreaCategoryDTO> GetAreaCategories()
+        {
+            
+        }
     }
 }
